22cc7ffeae594feb0fe8ffd3e98a9bb13d2c4724
Diego Leão <diegoleaovelame@hotmail.comgit commit -m closes>
2025-08-01 19:54:31 -0300

    closes #5, closes #6 - concluídas: lógica histórico das operações e finalização do histórico.

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -10,4 +10,20 @@ function calc(op) {
     }
 
     document.getElementById("output").value = output;
+    
+    var newHistory = "<div>" + num1 + " " + op + " " + num2 + " = " + output + "</div>";
+    //<div>" + num1 + " " + op + " " + num2 + " = " + output + "</div> concatenação de valores para fazer a saída do resultado do cáculo.
+
+    var history = document.getElementById("history");
+
+    history.innerHTML = newHistory + history.innerHTML;
+    //inverte a ordem do histórico, fazendo com que a última opração realizada seja a primeira a  ser exbida.
+    
+    //Inner html = inserir elementos html 
+    //+= acrescenta sem sobrescrever em cima do dado anterior. (foi removido).
+
+    if(history.children.length > 10) {
+        history.removeChild(history.childNodes[10]);
+    }
+    
 }
