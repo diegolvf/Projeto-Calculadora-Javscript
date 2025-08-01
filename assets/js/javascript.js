f2ab078feec59a3cc4bd44f11d3c78f5f78988f0
Diego Leão <diegoleaovelame@hotmail.comgit commit -m closes>
2025-08-01 17:46:59 -0300

    closes #4 - concluída: implementar as quatro operações matemáticas

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,7 +1,13 @@
-function calc() {
+function calc(op) {
     var num1 = parseFloat(document.getElementById("num1").value); //parseFloat() - conversão de texto para número.
     var num2 = parseFloat(document.getElementById("num2").value); //parseFloat() - conversão de texto para número.
-    var output = num1 + num2;
+    var output = 0;
+    switch(op) {
+        case '+' : output = num1 + num2; break
+        case '-' : output = num1 - num2; break
+        case '*' : output = num1 * num2; break
+        case '/' : output = num1 / num2; break
+    }
 
     document.getElementById("output").value = output;
 }
